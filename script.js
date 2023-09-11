cc0293ef49bbad97244d43a486b968467283327a
watrik1123 <104622692+watrik1123@users.noreply.github.com>
2023-09-12 00:57:33 +0300

    script.js

--- script.js
+++ script.js
@@ -1,24 +1,25 @@
-.buttons button {
-    width: 50px;
-    height: 50px;
-    font-size: 18px;
-    margin: 5px;
-    cursor: pointer;
-    background-color: #f2f2f2;
-    border: 1px solid #ccc;
-    border-radius: 5px;
+let display = document.getElementById('display');
+let currentValue = '';
+let currentOperator = '';
+
+function appendToDisplay(value) {
+    currentValue += value;
+    display.value = currentValue;
 }
 
-.buttons button:hover {
-    background-color: #ccc;
+function clearDisplay() {
+    currentValue = '';
+    currentOperator = '';
+    display.value = '';
 }
 
-/* Стили для поля ввода */
-input[type="text"] {
-    width: 100%;
-    padding: 10px;
-    margin-bottom: 10px;
-    font-size: 18px;
-    border: 1px solid #ccc;
-    border-radius: 5px;
+function calculateResult() {
+    try {
+        currentValue = eval(currentValue);
+        display.value = currentValue;
+    } catch (error) {
+        display.value = 'Ошибка';
+        currentValue = '';
+        currentOperator = '';
+    }
 }
