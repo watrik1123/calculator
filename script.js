2540fbb24cf64937309f69dee36188e255e996bf
watrik1123 <104622692+watrik1123@users.noreply.github.com>
2023-09-12 00:50:19 +0300

    script.js

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
-body {
-    font-family: Arial, sans-serif;
-    text-align: center;
+.buttons button {
+    width: 50px;
+    height: 50px;
+    font-size: 18px;
+    margin: 5px;
+    cursor: pointer;
     background-color: #f2f2f2;
-}
-
-.calculator {
-    width: 300px;
-    margin: 0 auto;
-    background-color: #fff;
     border: 1px solid #ccc;
     border-radius: 5px;
-    padding: 10px;
-    box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
 }
 
+.buttons button:hover {
+    background-color: #ccc;
+}
+
+/* Стили для поля ввода */
 input[type="text"] {
     width: 100%;
     padding: 10px;
@@ -21,16 +21,4 @@ input[type="text"] {
     font-size: 18px;
     border: 1px solid #ccc;
     border-radius: 5px;
-}
-
-.buttons button {
-    width: 60px;
-    height: 40px;
-    font-size: 18px;
-    margin: 5px;
-    cursor: pointer;
-}
-
-.buttons button:hover {
-    background-color: #ccc;
 }
